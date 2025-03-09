221c72877be6b3c7fc12f23a0ede955d88c92117
MadsPrip1 <madsprip235@gmail.com>
2025-03-09 11:21:26 +0100

    updated week4

---
+++ week4/optim.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+### -- set the job Name -- 
+#BSUB -J python_performance_optim
+
+### -- specify queue --
+#BSUB -q hpc
+
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 5
+
+### -- specify that we need 512MB of memory per core/slot --
+#BSUB -R "rusage[mem=10GB]"
+
+### -- select the machine to be XeonGold6226R --
+#BSUB -R "select[model==XeonGold6226R]"
+
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 1
+
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o bash_output/python_performance_optim%J.out
+#BSUB -e bash_output/python_performance_optim%J.err
+
+### -- Need to activate the python environment --
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+### -- run in the job --
+### -m cProfile will run the program
+### -s cumulative will sort by cumulative time (Cumulative is including time spent in sub-functions).
+python -m cProfile -s cumulative python_performance_optim.py /dtu/projects/02613_2025/data/locations/locations_100.csv
+
+
+
