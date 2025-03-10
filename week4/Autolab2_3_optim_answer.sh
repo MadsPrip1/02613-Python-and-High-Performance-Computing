092560e0d3af07fec9eab08506fdd5de8b8a60c7
Prip1 <mprip20@student.aau.dk>
2025-03-10 12:42:21 +0100

    updated week4

---
+++ week4/Autolab2_3_optim_answer.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+### -- set the job Name -- 
+#BSUB -J python_performance_2_3optim
+
+### -- specify queue --
+#BSUB -q hpc
+
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 5
+
+### -- specify that we need 512MB of memory per core/slot --
+#BSUB -R "rusage[mem=10GB]"
+
+### -- select the machine to be XeonGold6226R --
+#BSUB -R "select[model==XeonGold6226R]"
+
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 1
+
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o bash_output/python_performance_2_3optim%J.out
+#BSUB -e bash_output/python_performance_2_3optim%J.err
+
+### -- Need to activate the python environment --
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+
+### The following code is for the cprofiler
+### -m cProfile will run the program
+### -s cumulative will sort by cumulative time (Cumulative is including time spent in sub-functions).
+python -m cProfile -s cumulative python_performance_2_3optim.py input.csv
+
+### The following code is for the lineprofiler
+### kernprof -l  python_performance_2_3optim.py /dtu/projects/02613_2025/data/locations/locations_100.csv 
+### python -m line_profiler python_performance_2_3optim.py /dtu/projects/02613_2025/data/locations/locations_100.csv 
+
+
+
