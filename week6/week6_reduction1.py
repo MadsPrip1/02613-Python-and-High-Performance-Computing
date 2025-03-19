c964a36ac2503c0e8f05014fa9fb5226f0cfa9f8
MadsPrip1 <madsprip235@gmail.com>
2025-03-19 11:19:35 +0100

    updated week6

---
+++ week6/week6_reduction1.py
@@ -0,0 +1,51 @@
+import ctypes
+import multiprocessing as mp
+import numpy as np
+import sys
+
+from time import perf_counter as time
+from PIL import Image
+
+def init(shared_arr_):
+    global shared_arr
+    shared_arr = shared_arr_
+
+def tonumpyarray(mp_arr):
+    return np.frombuffer(mp_arr, dtype='float32')
+
+def reduce_step(args):
+    b, e, s, elemshape = args
+    arr = tonumpyarray(shared_arr).reshape((-1,) + elemshape)
+    #My code
+    arr[b] += np.sum(arr[b:e:s], axis=0)
+    
+    return arr[b]
+   
+ 
+if __name__ == '__main__':
+    n_processes = 1
+    chunk = 2
+    # Create shared array
+    data = np.load(sys.argv[1])
+    elemshape = data.shape[1:]
+    shared_arr = mp.RawArray(ctypes.c_float, data.size)
+    arr = tonumpyarray(shared_arr).reshape(data.shape)
+    len_arr = len(arr)
+    np.copyto(arr, data)
+    del data
+   
+    # Run parallel sum
+    t = time()
+    pool = mp.Pool(n_processes, initializer=init, initargs=(shared_arr,))
+
+    # Change the code below to compute a step of the reduction
+    # ---------------------------8<---------------------------
+    arr = pool.map(reduce_step, [(i, i + chunk, 1, elemshape) for i in range(0, len(arr), chunk)], chunksize=1)     # Need to make the parameters we have to a list because pool.map expect an iterable object
+
+    # Write output
+    print(time() - t)
+    final_image = arr[0]
+    
+    # final_image /= len_arr # For mean
+
+    Image.fromarray((255 * final_image.astype(float)).astype('uint8')).save('result reduction step 1.png')
